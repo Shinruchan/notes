5a079a02cb8f7be9f6de19457f19cbe75676e8cc
Aleks Drevensek <yamialeks@gmail.com>
2019-12-18 22:15:44 +0100

    init

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow, globalShortcut } = require('electron');
+
+function createWindow() {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  globalShortcut.register('CommandOrControl+R', function() {
+    win.reload();
+  });
+
+  // and load the index.html of the app.
+  win.loadFile('dist/index.html');
+  win.removeMenu();
+}
+
+app.on('ready', createWindow);
