93554942bbaeb010e92046c8eed6160f60abf7a8
Aleks Drevensek <yamialeks@gmail.com>
2019-12-26 00:56:25 +0100

    add theme switcher

--- app/store/store.js
+++ app/store/store.js
@@ -1,30 +1,19 @@
 import createStore from 'redux-zero';
-import uuid from 'uuid/v4';
 import { applyMiddleware } from 'redux-zero/middleware';
 import { persistMiddleware } from './persist.middleware';
 
 const INITIAL_STATE = {
-  notes: []
+  notes: [],
+  themes: ['midnight', 'morning'],
+  theme: 'midnight'
 };
 
-const notes = JSON.parse(localStorage.getItem('store-notes'));
-if (notes) INITIAL_STATE.notes = notes;
+const middlewares = applyMiddleware(
+  persistMiddleware(['notes', 'theme'], INITIAL_STATE)
+);
 
-/* INITIAL_STATE.notes = [
-  { id: uuid(), title: 'test', body: 'test \n aaaa' },
-  { id: uuid(), title: 'test', body: 'test \n aaaa' },
-  { id: uuid(), title: 'test', body: 'test \n aaaa' },
-  { id: uuid(), title: 'test2', body: 'test \n aawefwefaa', selected: true },
-  { id: uuid(), title: 'test', body: 'test \n aaaa' },
-  { id: uuid(), title: 'test', body: 'test \n aaaa' },
-  {
-    id: uuid(),
-    title:
-      'This one has a really really long title, longer than anything we ever expected',
-    body: 'test \n aaaa'
-  }
-]; */
-
-const middlewares = applyMiddleware(persistMiddleware);
+window.addEventListener('DOMContentLoaded', () => {
+  document.documentElement.setAttribute('theme', INITIAL_STATE.theme);
+});
 
 export const store = createStore(INITIAL_STATE, middlewares);
---
+++ app/views/Settings.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { useSelector, useAction } from 'redux-zero/react';
+import { Link } from 'react-router-dom';
+import { X } from 'react-feather';
+
+import { ThemeSample } from '../components';
+
+import styles from './Settings.css';
+import { Actions } from '../store';
+
+export const Settings = () => {
+  const themes = useSelector(({ themes }) => themes);
+  const selected = useSelector(({ theme }) => theme);
+  const changeTheme = useAction(Actions.changeTheme);
+
+  return (
+    <div className={styles.wrapper}>
+      <Link className={styles.close} to="/">
+        <X />
+      </Link>
+
+      <h2>Theme</h2>
+      <p>
+        Change the appearance of the application. Select one of the prepared
+        themes:
+      </p>
+      <br />
+      {themes.map((theme, i) => (
+        <div
+          className={`${styles.themeLine} ${
+            theme === selected ? styles.selected : undefined
+          }`}
+          key={i}
+          onClick={() => changeTheme(theme)}
+        >
+          <p>{theme.charAt(0).toUpperCase() + theme.slice(1)}</p>{' '}
+          <ThemeSample theme={theme} />
+        </div>
+      ))}
+    </div>
+  );
+};
